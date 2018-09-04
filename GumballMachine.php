7cf05f15facd5fd4629bc0eecea126571088ef62
nextmab <42300915+nextmab@users.noreply.github.com>
2018-09-04 22:56:21 +0300

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -1,22 +1,37 @@
+#!/archive/data1/bin/php -f
 <?php
+#
+# standalone PHP program 
+# (edit top line to point to local PHP executable)
+#
+# Submits GET request to search HST archive for given RA, Dec, and radius,
+# limit results to 10 records, 
+# returns data set name, RA, Dec, and Target name
+# as a comma-separated list
+# prints out column headings and data
+#
 
-class GumballMachine{
+# create GET request
 
-	private $gumballs;
+$request = "http://archive.stsci.edu/hst/search.php?";      //quotes url
+$request .= "RA=53.084&DEC=-27.873&radius=1.0&max_records=10&";
+$request .= "outputformat=CSV&action=Search"; 
+$request .= "&selectedColumnsCsv=sci_data_set_name,sci_ra,sci_dec,sci_targname";
 
+print "\nrequest = $request \n\n";
 
-	// Get the amount of gumballs still in the machine
-	public function getGumballs(){
-		return $this->gumballs;
-	}
+# download results from MAST as an array called $data
+# (ignore errors)
 
-	// Set the amount of gumballs in the machine
-	public function setGumballs($amount){
-		$this->gumballs = $amount;
-	}
+$data = @file($request);
 
-	// The user turns the wheel, machine dispenses gumball!
-	public function turnWheel(){
-		$this->setGumballs($this->getGumballs() - 1);
-	}
-}
+# print column headings (skip data types in next row)
+
+print "$data[0]\n\n";
+
+# now print search results
+
+$cnt = count($data);
+for ($i=2; $i<$cnt; $i++) print "$data[$i]\n";
+
+?>
