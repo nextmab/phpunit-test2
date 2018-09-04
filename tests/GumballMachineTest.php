b72eadedaf4be38fa4e32532408fd1d8b4f2f988
nextmab <42300915+nextmab@users.noreply.github.com>
2018-09-04 22:56:06 +0300

    Update GumballMachineTest.php

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -1,25 +1,37 @@
+#!/archive/data1/bin/php -f
 <?php
+#
+# standalone PHP program 
+# (edit top line to point to local PHP executable)
+#
+# Submits GET request to search HST archive for given RA, Dec, and radius,
+# limit results to 10 records, 
+# returns data set name, RA, Dec, and Target name
+# as a comma-separated list
+# prints out column headings and data
+#
 
+# create GET request
 
-require 'GumballMachine.php';
+$request = "http://archive.stsci.edu/hst/search.php?";      //quotes url
+$request .= "RA=53.084&DEC=-27.873&radius=1.0&max_records=10&";
+$request .= "outputformat=CSV&action=Search"; 
+$request .= "&selectedColumnsCsv=sci_data_set_name,sci_ra,sci_dec,sci_targname";
 
-class GumballMachineTest extends PHPUnit_Framework_TestCase
-{
-    public $gumballMachineInstance;
+print "\nrequest = $request \n\n";
 
-    public function setUp()
-    {
-        $this->gumballMachineInstance = new GumballMachine();
-    }
+# download results from MAST as an array called $data
+# (ignore errors)
 
-    public function testIfWheelWorks(){
-    	// Suppose we have 100 gumballs...
-    	$this->gumballMachineInstance->setGumballs(100);
+$data = @file($request);
 
-    	// ... And we turn the wheel once...
-    	$this->gumballMachineInstance->turnWheel();
+# print column headings (skip data types in next row)
 
-    	// ... we should now have 99 gumballs remaining in the machine right?
-    	$this->assertEquals(99, $this->gumballMachineInstance->getGumballs()); 
-    }
-}
+print "$data[0]\n\n";
+
+# now print search results
+
+$cnt = count($data);
+for ($i=2; $i<$cnt; $i++) print "$data[$i]\n";
+
+?>
